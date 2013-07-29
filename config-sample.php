fb6a84b3116ec63e3f1af969f011486d84350d9b
K.Sasaki <among753@gmail.com>
2013-07-29 15:18:28 +0900

    Add proxy setting

--- config-sample.php
+++ config-sample.php
@@ -8,3 +8,9 @@
 define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
 define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
 define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
+
+// Use proxy.
+define('PROXY_HOST', 'xxx.xxx.xxx.xxx');
+define('PROXY_PORT', '8080');
+//define('PROXY_USERNAME', 'username');
+//define('PROXY_PASSWORD', 'password');
